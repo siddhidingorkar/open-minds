775c27462139053a011a3558ff89f4d69a47d662
Siddhi Dingorkar <145720252+siddhidingorkar@users.noreply.github.com>
2024-06-26 21:54:35 +0530

    Update signup.jsx

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,40 +1,36 @@
-import React from 'react';
-import { Link } from 'react-router-dom'; // Import Link
-import '../index.css';
+import React from "react";
+import { Link } from "react-router-dom"; // Import Link
+import "../index.css";
 
 const signup = () => {
   return (
-    <div className="login-box" style={{marginTop:"30px"}}> 
+    <div className="login-box" style={{ marginTop: "30px" }}>
       <h2>Register</h2>
       <form>
-      <div className="user-box">
-          <input type="text" name="" required=""/>
+        <div className="user-box">
+          <input type="text" name="" required="" />
           <label>Name</label>
         </div>
         <div className="user-box">
-          <input type="text" name="" required=""/>
+          <input type="text" name="" required="" />
           <label>Email</label>
         </div>
         <div className="user-box">
-          <input type="text" name="" required=""/>
+          <input type="text" name="" required="" />
           <label>Phone No</label>
         </div>
-     
+
         <div className="user-box">
-          <input type="password" name="" required=""/>
+          <input type="password" name="" required="" />
           <label>Password</label>
         </div>
-       
-        <a href="#">
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
+
+        <button type="submit" className="submit-button">
           Submit
-        </a>
+        </button>
       </form>
-      <p style={{ color: 'white', marginTop: '20px' }}>
-        Already have an account? <Link to="/login">Login</Link>
+      <p style={{ color: "white", marginTop: "20px" }}>
+        Already have an account? <Link to="/Login">Login</Link>
       </p>
     </div>
   );
