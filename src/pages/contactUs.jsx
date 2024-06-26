46a99a8ee75731ee999c501330d45c25d8cfddca
Siddhi Dingorkar <145720252+siddhidingorkar@users.noreply.github.com>
2024-06-26 21:53:40 +0530

    Update contactUs.jsx

--- src/pages/contactUs.jsx
+++ src/pages/contactUs.jsx
@@ -1,43 +1,46 @@
-import React from 'react';
-import './contact.css'
-import Footer from '../components/Footer';
+import React from "react";
+import "./contact.css";
+import Footer from "../components/Footer";
 const ContactUs = () => {
   return (
     <>
-    <section id="contact">
-    <div class="contact-box">
-      <div class="contact-links">
-        <h2>CONTACT US</h2>
-        <ul style={{color:"white", marginTop:"80px"}}>
-        <li>Email: Rajat@email.com</li>
-        <li>Phone: +91 82562546</li>
-        <li>Address: 123 Main Street, pune, Maharashtra</li>
-      </ul>
-
-      </div>
-      <div class="contact-form-wrapper">
-        <form>
-          <div class="form-item">
-            <input type="text" name="sender" required/>
-            <label>Name:</label>
+      <section id="contact">
+        <div class="contact-box">
+          <div class="contact-links">
+            <h2>Contact Us</h2>
+            <ul
+              style={{
+                color: "white",
+                marginTop: "80px",
+                fontFamily: ["Inter", "sans-serif"].join(","),
+              }}
+            >
+              <li>Email: Rajat@email.com</li>
+              <li>Phone: +91 82562546</li>
+              <li>Address: 123 Main Street, pune, Maharashtra</li>
+            </ul>
           </div>
-          <div class="form-item">
-            <input type="text" name="email" required/>
-            <label>Email:</label>
+          <div class="contact-form-wrapper">
+            <form>
+              <div class="form-item">
+                <input type="text" name="sender" required />
+                <label>Name:</label>
+              </div>
+              <div class="form-item">
+                <input type="text" name="email" required />
+                <label>Email:</label>
+              </div>
+              <div class="form-item">
+                <textarea class="" name="message" required></textarea>
+                <label>Message:</label>
+              </div>
+              <button class="submit-btn">Send</button>
+            </form>
           </div>
-          <div class="form-item">
-            <textarea class="" name="message" required></textarea>
-            <label>Message:</label>
-          </div>
-          <button class="submit-btn">Send</button>  
-        </form>
-      </div>
-    </div>
-
-  </section>
-  <Footer/>
-  </>
-  
+        </div>
+      </section>
+      <Footer />
+    </>
   );
 };
 
